2fbe409d45f60e8ad4bd517ae20dd392b9f108fe
anjalivp <anjalivp92@gmail.com>
2026-03-02 22:28:20 +0530

    Update emotion_detection.py

--- final_project/emotion_detection.py
+++ final_project/emotion_detection.py
@@ -15,5 +15,33 @@ def emotion_detector(text_to_analyze):
     }
 
     response = requests.post(url, headers=headers, json=input_json)
+    response_dict = json.loads(response.text)
 
-    return response.text
+    # Extract emotions
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+
+    # Determine dominant emotion
+    emotion_scores = {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness
+    }
+
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    return {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness,
+        "dominant_emotion": dominant_emotion
+    }
